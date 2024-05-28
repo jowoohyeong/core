cc0097fdaf2a7459883e17db8e644aad17f34283
jwh <jwh5096@naver.com>
2024-05-28 17:29:31 +0900

    section 4 - 스프링 컨테이너와 스프링 빈

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,21 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+    // 다양한 설정 형식 지원 - 자바코드, xml
+    @Test
+    @DisplayName("XML 설정을 이용")
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
